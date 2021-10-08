412568e98b02150a23e7dd242efac2a895f8aaa3
ivand <ivandiscobolo@gmail.com>
2021-10-08 09:44:41 +0200

    Clase Login totalmente implementada

--- ejemplo1/src/main/java/com/example/ejemplo1/Clase.java
+++ ejemplo1/src/main/java/com/example/ejemplo1/Clase.java
@@ -24,4 +24,17 @@ public class Clase {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    //En clase Login, crea un método que reciba como parámetro otro objeto de clase
+    //Login y devuelva true si este objeto representa una conexión de un usuario que se
+    //ha conectado en una fecha posterior al la conexión del usuario representada por el
+    //objeto desde el que se invoca el método. Comprueba que funciona correctamente,
+    //comprobando desde la clase Programa que la primera conexión del usuario arcano
+    //ha sido en una fecha posterior a la primera conexión del usuario tudela
+
+    public boolean conexionPosterior(Clase otro){
+        if(otro.fecha > this.fecha){
+            return true;
+        } return false;
+    }
 }
--- ejemplo1/src/main/java/com/example/ejemplo1/Programa.java
+++ ejemplo1/src/main/java/com/example/ejemplo1/Programa.java
@@ -6,4 +6,33 @@ public class Programa {
             1289013000000L , 1289016000000L};
     static String USUARIO[] = {"carchimp", "tudela", "arcano", "totencar",
             "ramporo", "tudela", "arcano", "carchimp", "tudela", "totencar"};
+
+
+    public static void main(String args[]) {
+        //Clase obj1 = new Clase(FECHA[5], USUARIO[5]);
+        //Clase obj2 = new Clase(FECHA[6], USUARIO[6]);
+
+        int indice1 = 0;
+        int indice2 = 0;
+
+        for(int i = 0; i < USUARIO.length; i++){
+            if(USUARIO[i].equals("tudela")){
+                indice1 = i;
+                break;
+            }
+        }
+
+        for(int i = 0; i < USUARIO.length; i++){
+            if(USUARIO[i].equals("arcano")){
+                indice2 = i;
+                break;
+            }
+        }
+
+        Clase obj1 = new Clase(FECHA[indice1], USUARIO[indice1]);
+        Clase obj2 = new Clase(FECHA[indice2], USUARIO[indice2]);
+
+            System.out.println(obj1.conexionPosterior(obj2));
+
+    }
 }
