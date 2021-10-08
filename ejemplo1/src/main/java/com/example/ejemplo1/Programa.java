c46fcb899552b4d95ab6e79dd950c4984f12b4ba
ivand <ivandiscobolo@gmail.com>
2021-10-08 10:21:59 +0200

    Programa finalizado

--- ejemplo1/src/main/java/com/example/ejemplo1/Programa.java
+++ ejemplo1/src/main/java/com/example/ejemplo1/Programa.java
@@ -1,16 +1,28 @@
 package com.example.ejemplo1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Programa {
     static long FECHA[]={1289001000000L , 1289002000000L , 1289004000000L ,
             1289006000000L , 1289007000000L, 1289008000000L , 128900900000L , 1289011000000L ,
             1289013000000L , 1289016000000L};
     static String USUARIO[] = {"carchimp", "tudela", "arcano", "totencar",
             "ramporo", "tudela", "arcano", "carchimp", "tudela", "totencar"};
+    static long FECHA_ACTUAL = 1289025000000L;
 
 
     public static void main(String args[]) {
-        //Clase obj1 = new Clase(FECHA[5], USUARIO[5]);
-        //Clase obj2 = new Clase(FECHA[6], USUARIO[6]);
+
+        List<Clase> lista_login = new ArrayList<Clase>();
+
+        for(int i = 0; i < FECHA.length; i++){
+            lista_login.add(new Clase(FECHA[i], USUARIO[i]));
+        }
 
         int indice1 = 0;
         int indice2 = 0;
@@ -32,7 +44,41 @@ public class Programa {
         Clase obj1 = new Clase(FECHA[indice1], USUARIO[indice1]);
         Clase obj2 = new Clase(FECHA[indice2], USUARIO[indice2]);
 
-            System.out.println(obj1.conexionPosterior(obj2));
+        System.out.println(obj1.conexionPosterior(obj2));
+
+        Map<String, Long> tiempo_medio = new HashMap<String, Long>();
+        Set<String> usuarios = new HashSet<String>();
+        for (String nombre:
+             USUARIO) {
+            usuarios.add(nombre);
+        }
+
+        System.out.println(usuarios);
+
+
+        int contador = 0;
+        long tiempos[] = new long[3];
+        long media = 0;
+        for (String nombre:
+                usuarios) {
+            for(int i = 0; i< USUARIO.length; i++){
+                if(nombre.equals(USUARIO[i])){
+                    tiempos[contador] = FECHA[i];
+                    contador++;
+                }
+            }
+
+            tiempos[contador] = FECHA_ACTUAL;
+            for(int i = 0; i < tiempos.length - 1;i++){
+                media = tiempos[i + 1] - tiempos[i];
+            }
+
+            media = media/tiempos.length;
+            tiempo_medio.put(nombre, media);
+            contador = 0;
+        }
+
+        System.out.println(tiempo_medio);
 
     }
 }
